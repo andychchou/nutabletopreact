7ec10210e7f4ddd58eef6a609ce0661e97eec33c
Andy Chou <andychchou@gmail.com>
2021-09-21 19:38:08 -0700

    Redux Initialization

---
+++ src/components/AboutComponent.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
+
+function PartnerList(props) {
+    const partners = props.partners.partners.map(partner => {
+        return (
+            <Fade in key={partner.id}>
+                <Media tag="li">
+                    <RenderPartner partner={partner}/>
+                </Media>
+            </Fade>
+        );
+    });
+    if (props.partners.isLoading) {
+        return <Loading />;
+    }
+    if (props.partners.errMess) {
+        console.log(props.partners.errMess);
+        return (
+            <div className="col">
+                <h4>{props.partners.errMess}</h4>
+            </div>
+        );
+    }
+    return (
+        <div className="col mt-4">
+            <Media list>
+                <Stagger in>{partners}</Stagger>
+            </Media>
+        </div>
+    );
+}
+
+function RenderPartner({partner}) {
+    if (partner) {
+        return (
+            <React.Fragment>
+                <Media object src={baseUrl + partner.image} alt={partner.name} width="150" />
+                <Media body className="ml-5 mb-4">
+                    <Media heading>{partner.description}</Media>
+                </Media>
+            </React.Fragment>
+        );
+    }
+    
+    return <div></div>
+}
+
+function About(props) {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>About Us</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>About Us</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row row-content">
+                <div className="col-sm-6">
+                    <h3>Header</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div className="col-sm-6">
+                    <Card>
+                        <CardHeader className="bg-primary text-white"><h3>Card Header</h3></CardHeader>
+                        <CardBody>
+                            <dl className="row">
+                                <dt className="col-6">Founded</dt>
+                                <dd className="col-6">Date here</dd>
+                                <dt className="col-6">Row 2</dt>
+                                <dd className="col-6">Row 2 Data</dd>
+                                <dt className="col-6">Row 3</dt>
+                                <dd className="col-6">Row 3 Data</dd>
+                                <dt className="col-6">Row 4</dt>
+                                <dd className="col-6">Row 4 Data</dd>
+                            </dl>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+            <div className="row row-content">
+                <div className="col-12">
+                    <h3>Community Partners</h3>
+                </div>
+                <div className="col mt-4">
+                    <PartnerList partners={props.partners}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/redux/ActionCreators.js
@@ -0,0 +1,230 @@
+import * as ActionTypes from './ActionTypes';
+import { baseUrl } from '../shared/baseUrl';
+
+//nested arrow functions enabled by redux thunk
+export const fetchCampsites = () => dispatch => {
+
+    dispatch(campsitesLoading());
+    //a call to fetch will return a promise
+    return fetch(baseUrl + 'campsites')
+        .then(response => {
+            //when promise resolve
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            //when promise reject
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(campsites => dispatch(addCampsites(campsites)))
+        .catch(error => dispatch(campsitesFailed(error.message)));
+};
+
+//not using redux thunk, just returns an action object
+export const campsitesLoading = () => ({
+    type: ActionTypes.CAMPSITES_LOADING
+});
+
+export const campsitesFailed = errMess => ({
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+});
+
+export const addCampsites = campsites => ({
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
+});
+
+//fetching comments
+export const fetchComments = () => dispatch => {
+    return fetch(baseUrl + 'comments')
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
+};
+
+export const commentsFailed = errMess => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errMess
+});
+
+export const addComments = comments => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+        campsiteId: campsiteId,
+        rating: rating,
+        author: author,
+        text: text
+    };
+    newComment.date = new Date().toISOString();
+
+    return fetch(baseUrl + 'comments', {
+            method: "POST",
+            body: JSON.stringify(newComment),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log('post comment', error.message);
+            alert('Your comment could not be posted\nError: ' + error.message);
+        })
+};
+
+//fetching promotions
+export const fetchPromotions = () => dispatch => {
+
+    dispatch(promotionsLoading());
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(promotions => dispatch(addPromotions(promotions)))
+        .catch(error => dispatch(promotionsFailed(error.message)));
+};
+
+export const promotionsLoading = () => ({
+    type: ActionTypes.PROMOTIONS_LOADING
+});
+
+export const promotionsFailed = errMess => ({
+    type: ActionTypes.PROMOTIONS_FAILED,
+    payload: errMess
+});
+
+export const addPromotions = promotions => ({
+    type: ActionTypes.ADD_PROMOTIONS,
+    payload: promotions
+});
+
+//fetching partners
+export const fetchPartners = () => dispatch => {
+
+    dispatch(partnersLoading());
+
+    return fetch(baseUrl + 'partners')
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(partners => dispatch(addPartners(partners)))
+        .catch(error => dispatch(partnersFailed(error.message)));
+};
+
+export const partnersLoading = () => ({
+    type: ActionTypes.PARTNERS_LOADING
+});
+
+export const partnersFailed = errMess => ({
+    type: ActionTypes.PARTNERS_FAILED,
+    payload: errMess
+});
+
+export const addPartners = partners => ({
+    type: ActionTypes.ADD_PARTNERS,
+    payload: partners
+});
+
+export const postFeedback = (feedback) => () => {
+    const newFeedback = {
+        firstName: feedback.firstName,
+        lastName: feedback.lastName,
+        phoneNum: feedback.phoneNum,
+        email: feedback.email,
+        agree: feedback.agree,
+        contactType: feedback.contactType,
+        feedback: feedback.feedback
+    };
+    return fetch(baseUrl + 'feedback', {
+            method: "POST",
+            body: JSON.stringify(newFeedback),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(newFeedback => alert(`Thank you for your feedback!\n${JSON.stringify(newFeedback)}`))
+        .catch(error => {
+            console.log('post feedback', error.message);
+            alert('Your feedback could not be posted\nError: ' + error.message);
+        })
+};
