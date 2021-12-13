b2996811066375049997c0faa74d653a6b6a1364
Andy Chou <andychchou@gmail.com>
2021-12-12 19:10:52 -0800

    deploy setup

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,3 @@
 // export const baseUrl = 'https://ttt-db.herokuapp.com/';
-export const baseUrl = 'https://localhost:3443/';
+// export const baseUrl = 'https://localhost:3443/';
+export const baseUrl = 'https://tttservernode.herokuapp.com/';
